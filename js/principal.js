1f545c235e5271ac7159ecb2278eed53970c0c9f
alexbelchior <alexbelchior@gmail.com>
2020-09-02 23:26:11 -0300

    Criação versionamento

---
+++ js/principal.js
@@ -0,0 +1,43 @@
+var titulo = document.querySelector("h1");
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    if (peso < 0 || peso > 200) {
+        console.log("Peso Inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    var alturaEhValida = true;
+    if (altura < 0 || altura > 3.00) {
+        console.log("Altura Inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+}
+
+
+
+
+console.log(imc);
